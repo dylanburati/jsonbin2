480f0a2f46a6ff97920174d9839b924e75510d6e
Dylan Burati <dylanburati@protonmail.com>
2020-08-18 01:45:41 -0400

    haproxy tweaks

--- docker/haproxy/entry.sh
+++ docker/haproxy/entry.sh
@@ -7,13 +7,19 @@ if [ ! -f "$FULLCHAIN" ]; then
     DOMAIN_ARGS="$DOMAIN_ARGS -d $DN";
   done
 
-  trap 'echo bootstrap received hangup' HUP
-  socat TCP-LISTEN:80,fork TCP:localhost:88 &
-  acme.sh --issue --standalone $DOMAIN_ARGS --httpport 88 && \
-  acme.sh --install-certs -d "$PRIMARY" \
-    --fullchain-file "$FULLCHAIN" \
-    --reloadcmd "kill -HUP 1"
+  trap 'echo bootstrap: received hangup' HUP
+  socat TCP-LISTEN:80,reuseaddr,fork TCP:localhost:88 &
+  acme.sh --issue --standalone $DOMAIN_ARGS --httpport 88 || exit 1
   kill %1
+  echo 'bootstrap: killed socat'
+  while netstat -tln | grep '0.0.0.0:80'; do
+    echo 'bootstrap: waiting for port 80 to be available'
+    sleep 0.5
+  done
+  acme.sh --install-cert -d "$PRIMARY" \
+    --fullchain-file "$FULLCHAIN" \
+    --key-file "$FULLCHAIN.key" \
+    --reloadcmd "kill -HUP 1"
 fi
 
 crond
