1af7305031a8aead08280249a12beb0bb494a1df
Dylan Burati <dylanburati@protonmail.com>
2020-11-25 23:45:31 -0500

    remove haproxy, use SSH deploy

--- scripts/gen-selfsigned.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-mkdir -p ~/local-certs
-cd ~/local-certs
-
-openssl req \
-  -newkey rsa:2048 -nodes -keyout fullchain.pem.key \
-  -x509 -days 365 -out fullchain.pem \
-  -subj "/C=US/O=localhost/OU=Development/CN=*.localhost.dev"
